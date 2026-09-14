e7d2a04702df086a2b6a1730101daa995b2c04de
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2026-09-14 21:31:25 +0300

    fix lint 5

--- tests/access-and-teams.test.tsx
+++ tests/access-and-teams.test.tsx
@@ -1,6 +1,7 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from "vitest";
 
-describe('access-and-teams', () => {
-	it('runs a simple test', () => {
-		expect(1 + 1).toBe(2);
-	});
+describe("access-and-teams", () => {
+  it("runs a simple test", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
