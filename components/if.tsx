d100182f6bc7d80ed32f45332f9bc77e5b97396c
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2026-09-15 15:38:04 +0300

    resolve any lint

--- components/if.tsx
+++ components/if.tsx
@@ -1,5 +1,4 @@
-
-export function If(props: { children: React.ReactNode, condition: any }) {
-    if(props.condition) return props.children;
-    return null;
+export function If(props: { children: React.ReactNode; condition: unknown }) {
+  if (props.condition) return props.children;
+  return null;
 }
